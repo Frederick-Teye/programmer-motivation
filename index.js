cf3e9e95a66a3a1d8bb61bfbc049f5685f869aed
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2023-11-28 22:02:24 +0000

    Populate the js file with two variables
    The first variable selected div.tools-info-images-section and the
    second variable selected all the div.tools-info-image-coat items in it.

---
+++ index.js
@@ -0,0 +1,3 @@
+// Select the container and items
+const displayContainer = document.querySelector('.tools-info-images-section');
+const items = displayContainer.querySelectorAll('.tools-info-image-coat');
