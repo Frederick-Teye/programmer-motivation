23eee978ec7bc518854fe836d68592b99cebbb88
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2023-11-29 22:50:20 +0000

    Add a function to display a batch of items

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 // Select the container and items
 const displayContainer = document.querySelector('.tools-info-images-section');
 const items = displayContainer.querySelectorAll('.tools-info-image-coat');
+
+// Number of items to display at a time
+const batchSize = 5;
+
+// Index to track currently displayed batch
+let currentIndex = 0;
+
+// Function to display a batch of items
+function displayBatch(startIndex) {
+    // Hide all items
+    items.forEach(item => {
+        item.style.display = 'none';
+    });
+
+    // Display the next batch
+    for (let i = startIndex; i < startIndex + batchSize; i++) {
+        if (items[i]) {
+            items[i].style.display = 'block';
+        }
+    }
+}
